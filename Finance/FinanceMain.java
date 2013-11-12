d3a4066f311497e35aaeebb0f89ed176e4f8d08a
Julie <junghyunjulie@gmail.com>
2013-11-12 17:19:53 -0500

    new finance main class

---
+++ Finance/FinanceMain.java
@@ -0,0 +1,16 @@
+/* HMSM - Homless Shleter Management System
+ * Modified Date: 12/11/2013
+ * Author: Julie Yu
+ * Wrapper for finance class
+ */
+
+public class FinanceMain
+{
+	public FinanceMain(int emplID)
+	{
+		//Chceck emplID with database -> match with finance department?
+		
+		//Ch
+	}
+
+}
